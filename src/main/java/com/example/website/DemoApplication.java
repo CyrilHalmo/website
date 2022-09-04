e2e4a47fcd2fb0f6c79225de45e6b1b72fca1eec
Cyril Halmo <cyril.halmo@gmx.ch>
2022-09-04 20:44:51 +0200

    Added html Template

--- src/main/java/com/example/website/DemoApplication.java
+++ src/main/java/com/example/website/DemoApplication.java
@@ -3,17 +3,23 @@ package com.example.website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class DemoApplication {
 	
 	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
+	String home(Model model) {
+		model.addAttribute("subtitle", "test");
+		return "index";
+	}
+	
+	@RequestMapping("/de")
+	String homeDE(Model model) {
+		model.addAttribute("subtitle", "Wenn Programmieren auf Passion trifft");
+		return "index";
 	}
 
 	public static void main(String[] args) {
